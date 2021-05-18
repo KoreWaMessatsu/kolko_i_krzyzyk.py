5c29973efa4e60dba543afbd2aba7cfdac9f4ebb
KoreWaMessatsu <wojciech.grzegorczyk91@gmail.com>
2021-05-18 21:54:08 +0200

    #Another day, another step in correct direction

--- Gra.py
+++ Gra.py
@@ -2,39 +2,45 @@
 import pprint
 import random
 
-the_board_dictionary = {"1A": " ", "1B": " ", "1C": " ", "2A": " ", "2B": " ", "2C": " ", "3A": " ", "3B": " ", "3C": " "}        
+the_board_dictionary = {"1A": " ",  "1B": " ", "1C": " ", "2A": " ", "2B": " ", "2C": " ", "3A": " ", "3B": " ", "3C": " "}        
 
-def is_field_empty(field_to_check):
-    return the_board_dictionary[field_to_check] == " "
+
+def is_field_empty(field_to_check, board_dictionary):
+    if (field_to_check in board_dictionary):
+        return board_dictionary[field_to_check] == " "
+    else:
+        print("Wybierz istniejące pole!")
+        return False
 
 def all_fields_are_nonempty(the_board_dicitionary):
     return not any(f == " " in f for f in the_board_dicitionary)
 
-def player_move():
-    player_choice = input("Wybierz puste pole: ").upper()
-    the_board_dictionary[player_choice] = 'X'
-    draw_the_board(the_board_dictionary)
-    main_game_loop(player_choice)
+def get_player_move(gamefield):
+    temporary_choice = ""
+    while not is_field_empty(temporary_choice, gamefield):
+        temporary_choice = input("Wybierz puste pole: ").upper()
+    return temporary_choice
 
-def bot_move():
-    bot_choice = random.choice(list(the_board_dictionary))
-    the_board_dictionary[bot_choice] = 'O'
-    draw_the_board(the_board_dictionary) 
-    main_game_loop(bot_choice)
+def get_bot_move(gamefield):
+    temporary_bot_choice = ""
+    while not is_field_empty(temporary_bot_choice, gamefield):
+            temporary_bot_choice = random.choice(list(the_board_dictionary))
+    return temporary_bot_choice
 
-def main_game_loop(player_choice, bot_choice, field_to_check):
-    player_count_move = 0
-    bot_count_move = 0
-    #each_count_move = 0
-    while player_count_move <= 5:
-        if not(is_field_empty(field_to_check)):
-            player_count_move += 1
-            return player_move
-        elif not(is_field_empty(field_to_check)): 
-            bot_count_move += 1
-            return bot_move
-        else:
-            pass
+def main_game_loop():
+    count_player_move = 0
+    count_bot_move = 0
+
+    while count_player_move <= 5:
+        if count_player_move == count_bot_move:
+            current_player_move = get_player_move(the_board_dictionary)
+            the_board_dictionary[current_player_move] = 'X'
+            count_player_move += 1
+        else: #count_player_move <= count_bot_move:     
+            current_bot_move = get_bot_move(the_board_dictionary)
+            the_board_dictionary[current_bot_move] = "O"
+            count_bot_move += 1
+        draw_the_board(the_board_dictionary)
 
 def draw_the_board(the_board_dictionary):
     print("   " + "A" + " B" + " C")
@@ -44,80 +50,8 @@ def draw_the_board(the_board_dictionary):
     print("  -------")
     print("3 |" + the_board_dictionary["3A"] + "|" + the_board_dictionary["3B"] + "|" + the_board_dictionary["3C"] + "|")
 
-main_game_loop(player_move, bot_move)
+
+main_game_loop()
 pprint.pprint(the_board_dictionary)
-
-
-
-
-'''
-
-Dodać funkcje:
-
-human_move(fields) nic nie zwraca
-bot_move(fields) nic nie zwraca (pisze po planszy)
-game_ended(fields) zwracać bool (true/fals)
-
-madre sprawdzenie warunkow zwyciestwa
-przerobic petle gry, bo teraz w jednej iteracji robia sie dwa ruchy
-
-while warunki:
-    ruch(plansza)
-
-def ruch()
-    if (ruch_gracza):
-        human_move()
-    else 
-        bot_move
-
-ale wtedy musisz sobie trzymac zmienna bool ktora sie zmienia na wartosc przeciwna 
-(napisac funkcje do zmiany na wartosc przeciwna chyba, ze python juz cos takiego ma (reverse boolean google))
-
-
-
-def main_game_loop():
-    player_move = 0
-    bot_move = 0
-    all_moves = 0
-
-    player_choice = input("Które pole chcesz zaznaczyć: ").upper()
-    the_board_dictionary[player_choice] = 'X'
-    draw_the_board(the_board_dictionary)
-
-    bot_choice = random.choice(list(the_board_dictionary))
-    the_board_dictionary[bot_choice] = 'O'
-    draw_the_board(the_board_dictionary)   
-
-    while player_move <= 3 and bot_move <= 3:
-        player_choice = input("Wybierz puste pole: ").upper()
-
-        #if (not all_fields_are_nonempty):
-        #    print("dupa")
-
-        all_moves += 1
-        if (all_moves == 8):
-            break
-        while not(is_field_empty(player_choice)):
-            player_choice = input("Wybierz puste pole: ").upper()
-
-        the_board_dictionary[player_choice] = 'X'
-
-        bot_choice = random.choice(list(the_board_dictionary))
-
-        all_moves += 1
-        if (all_moves == 8):
-            break    
-        while not(is_field_empty(bot_choice)):
-            bot_choice = random.choice(list(the_board_dictionary))
-
-        the_board_dictionary[bot_choice] = 'O'
-        draw_the_board(the_board_dictionary)
-
-        player_move += 1
-        bot_move += 1
-
-    print("Koniec gry")
-
-'''
 
 
